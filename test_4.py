6ab249ac21feb03e4f588313ac9dec238e46c599
Timofey Simonov <timofeysimonov@mail.ru>
2025-01-22 04:00:20 +0300

    async queue suffering goes on

--- test_4.py
+++ test_4.py
@@ -13,40 +13,49 @@ class Priority:
     COOLDOWN = 3
 
 
-
 class ProxyQueue(heapdict):
     def __init__(self, proxy_list=[], cooldown_duration=300):
         self.cooldown_duration = cooldown_duration
-        self.blocked = False
-        self.cooldown_end_timestamps = []
+        self.cooldown_end_queue = asyncio.Queue()
         self.queue_semaphore = asyncio.Semaphore(1)
-        self.cooldown_list_semaphore = asyncio.Semaphore(1)
+        self.cooldown_scheduler = asyncio.locks.Event()
         super(ProxyQueue, self).__init__()
         if proxy_list:
-            self.add_proxies(proxy_list)
+            asyncio.run(self.add_new_proxies(proxy_list))
 
-    def add_proxies(self, proxy_list):
+    async def add_new_proxies(self, proxy_list):
         for proxy in proxy_list:
             if not proxy in self:
                 self[proxy] = Priority.UNCHECKED
 
-    async def popitem(self, key):
-        priority = super(ProxyQueue, self).peekitem(key)[1]
-        if priority == Priority.COOLDOWN:
-            delay = time.time()
-            await asyncio.sleep()
+    async def popitem(self):
+        async with self.queue_semaphore:
+            priority = super(ProxyQueue, self).peekitem()[1]
+            if priority == Priority.COOLDOWN:
+                delay = await self.cooldown_end_queue.get() + time.time()
+                wait_task = asyncio.create_task(self.cooldown_scheduler.wait())
+                asyncio.create_task(self._unlock_queue_delayed(delay))
+                await wait_task
+            item = super(ProxyQueue, self).popitem()[0]
         return item
 
     def __setitem__(self, key, value):
         if value == Priority.COOLDOWN:
             asyncio.create_task(self._change_priority_delayed(key, Priority.ACTIVE))
-            self.cooldown_end_timestamps.append(time.time() + self.cooldown_duration)
+            self.cooldown_end_queue.put_nowait(time.time() + self.cooldown_duration)
+        elif not self.cooldown_scheduler.is_set():
+            self.cooldown_scheduler.set()
         super(ProxyQueue, self).__setitem__(key, value)
     
     async def _change_priority_delayed(self, item, new_priority: Priority):
         await asyncio.sleep(self.cooldown_duration)
-        self.cooldown_end_timestamps.pop(0)
+        await self.cooldown_end_queue.get()
         self[item] = new_priority
+
+    async def _unlock_queue_delayed(self, delay):
+        await asyncio.sleep(delay)
+        if not self.cooldown_scheduler.is_set():
+            self.cooldown_scheduler.set()
 
 
 
@@ -78,14 +87,23 @@ class PrioritizedItem:
 
 
 async def main():
+    '''
     q = PriorityQueue()
     q.put(PrioritizedItem(Priority.UNCHECKED, 'unch'))
     q.put(PrioritizedItem(Priority.COOLDOWN, 'frg'))
     timer = time.time()
     while time.time() - timer < 10:
         print(q.queue)
-        q.
         await asyncio.sleep(0.5)
+    '''
+    q = ProxyQueue([], 10)
+    print(q.heap)
+    await q.add_new_proxies(['proxy_1', 'proxy_2'])
+    print(q.heap)
+    print(await q.popitem())
+    print(await q.popitem())
+    print(q.heap)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
